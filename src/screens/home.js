d32a1edec8a95bc4a8e71750b3a97bf8e8e531f3
utsav100 <88386525+utsav100@users.noreply.github.com>
2021-08-06 14:16:13 +0530

    home

--- src/screens/home.js
+++ src/screens/home.js
@@ -2,37 +2,37 @@ import React from "react";
 import { Hero } from "../components/Hero";
 import { ServicesList } from "../components/Services";
 import { AnimatedSection } from "../components/AnimatedSection";
-import image from "../assets/MaskGroup2.jpg";
+import image from "../assets/back1.jpg";
 const services = [
   {
-    title: "Interior Design",
+    title: "Primary school",
     text:
-      "You will learn fundamentals of interior design, tools and theories used to kickstart your career in this space",
+      "You will learn primary education.",
     color: "F1F11E",
-    image: require("../assets/yellow.jpg"),
+    image: require("../assets/back.jpg"),
     gradient: ["#D68800", "#CB5555"]
   },
-  {
-    title: "Fashion Design",
-    text:
-      "You will learn fundamentals of interior design, tools and theories used to kickstart your career in this space",
-    color: "27F8E8",
-    image: require("../assets/blue.jpg"),
-    gradient: ["#0054D6", "#55ACCB"]
-  },
-  {
-    title: "Graphic Design",
-    text:
-      "You will learn fundamentals of interior design, tools and theories used to kickstart your career in this space",
-    color: "EF78E8",
-    image: require("../assets/purple.jpg"),
-    gradient: ["#D600D6", "#A455CB"]
-  }
+  // {
+  //   title: "Fashion Design",
+  //   text:
+  //     "You will learn fundamentals of interior design, tools and theories used to kickstart your career in this space",
+  //   color: "27F8E8",
+  //   image: require("../assets/blue.jpg"),
+  //   gradient: ["#0054D6", "#55ACCB"]
+  // },
+  // {
+  //   title: "Graphic Design",
+  //   text:
+  //     "You will learn fundamentals of interior design, tools and theories used to kickstart your career in this space",
+  //   color: "EF78E8",
+  //   image: require("../assets/purple.jpg"),
+  //   gradient: ["#D600D6", "#A455CB"]
+  // }
 ];
 
 const Home = () => (
   <div>
-    <Hero title1="Design" title2="School" image={image} />
+    <Hero title1="Hmghaswala" title2="School" image={image} />
     <AnimatedSection>
       <ServicesList items={services} />
     </AnimatedSection>
